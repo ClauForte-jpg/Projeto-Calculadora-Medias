7e307a8f0632832f1fa26ee5631d5a46f81b6ec7
ClauForte-jpg <claudiaforte789@gmail.com>
2025-05-09 14:41:37 -0300

    Atualização do site

--- main.js
+++ main.js
@@ -62,8 +62,7 @@ function atualizaMediafinal() {
     const mediaFinal = calculaMediaFinal();
 
     document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML =
-        mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal() {
